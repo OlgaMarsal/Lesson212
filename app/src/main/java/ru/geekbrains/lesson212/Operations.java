63a0fa8780f665525cce15fef16fe216c2372a1a
OlgaMArsal <inconcretehome@gmail.com>
2021-01-19 07:17:36 +0300

    ConstraitLayout calculator lesson 212 - lesson 3

---
+++ app/src/main/java/ru/geekbrains/lesson212/Operations.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.lesson212;
+
+import android.widget.TextView;
+
+public class Operations {
+    public int getOperationDisplay(TextView numbersText, String s) {
+        return 0;
+    }
+
+    public int getOperationDislay() {
+    }
+
+    public void display(String number, String toString) {
+    }
+
+    public void math(String s) {
+    }
+
+    public void point(String point) {
+    }
+}
